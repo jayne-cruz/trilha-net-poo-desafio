3f41e1f002b69408a107e7fa58b0bf2d3357382c
Jayne Cruz <152091678+jayne-cruz@users.noreply.github.com>
2023-12-14 23:25:08 -0300

    Update Smartphone.cs
    Adicionado // Correção realizada!

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -4,7 +4,7 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
 
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        // Correção realizada!
         public string Numero { get; set; }
         private string Modelo { get; set; }
         private string IMEI { get; set; }
@@ -14,7 +14,7 @@ namespace DesafioPOO.Models
 
         public Smartphone(string numero, string modelo, string imei, int memoria)
         {
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            // Correção realizada!
             Numero = numero;
             Modelo = modelo;
             IMEI = imei;
